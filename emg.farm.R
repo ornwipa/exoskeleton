f3f83093171a81ccd37e7cf5d7e2bf3801616797
ornwipa <fahornwipa@gmail.com>
2020-08-04 09:58:34 -0600

    linear regression to adjust confounders, add .gitignore for .Rhistory

--- emg.farm.R
+++ emg.farm.R
@@ -32,3 +32,30 @@ anova(lmer(log(X10thPct) ~ Condition + (1|Participant) + (1|Task), data = emg.fa
 anova(lmer(log(X50thPct) ~ Condition + (1|Participant) + (1|Task), data = emg.farm)) # 0.0248 *
 anova(lmer(log(X90thPct) ~ Condition + (1|Participant) + (1|Task), data = emg.farm)) # 0.1854
 
+#### Re-analysis using linear regression with condition as predictor, sex, age, BMI and ID as confounding factors
+
+setwd("~/Desktop/EMGfieldData")
+emg.farm <- na.omit(read.csv(file.choose(), header=TRUE)) # emg_farm.csv
+demographics <- na.omit(read.csv(file.choose(), header=TRUE)) # demographics.csv
+
+# merge demographics to EMG data set
+rows <- length(t(emg.farm))/length(emg.farm) # number of rows
+sex <- rep(NA,rows)
+age <- rep(NA,rows)
+bmi <- rep(NA, rows)
+for (row in 1:rows) {
+  subj <- emg.farm[row,1]
+  for (ID in 1:18) {
+    if (demographics$subject[ID]==subj) {
+      sex[row] <- demographics$sex[ID]
+      age[row] <- demographics$age[ID]
+      bmi[row] <- demographics$BMI[ID]
+    }
+  }
+}
+data.merge <- data.frame(cbind(emg.farm, sex, age, bmi))
+
+# statistical analysis
+summary(lm(X10thPct ~ Condition + factor(sex) + age + bmi + Participant, data = data.merge)) # 0.0474
+summary(lm(X50thPct ~ Condition + factor(sex) + age + bmi + Participant, data = data.merge)) # 0.0501
+summary(lm(X90thPct ~ Condition + factor(sex) + age + bmi + Participant, data = data.merge)) # 0.30
